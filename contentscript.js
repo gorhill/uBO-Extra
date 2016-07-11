e1497a8b34e5b263dd234e4af3e3986a847150c4
gorhill <rhill@raymondhill.net>
2016-07-11 13:52:11 -0400

    code review for last commit

--- contentscript.js
+++ contentscript.js
@@ -154,6 +154,7 @@ script.textContent = "\
         }                                                                \n\
     };                                                                   \n\
     var WebSocket = function(url, protocols) {                           \n\
+        'native';                                                        \n\
         if (                                                             \n\
             window.location.protocol === 'https:' &&                     \n\
             url.lastIndexOf('ws:', 0) === 0                              \n\
@@ -305,7 +306,7 @@ script.textContent = "\
     WebSocket.OPEN = 1;                                                  \n\
     WebSocket.CLOSING = 2;                                               \n\
     WebSocket.CLOSED = 3;                                                \n\
-    window.WebSocket = WebSocket.bind(window);                           \n\
+    window.WebSocket = WebSocket;                                        \n\
     var me = document.currentScript;                                     \n\
     if ( me && me.parentNode !== null ) {                                \n\
         me.parentNode.removeChild(me);                                   \n\
