0b4fb0c3d75aa977d35aa47172a51a7ca00a3535
gorhill <rhill@raymondhill.net>
2017-08-09 12:14:06 -0400

    fix https://www.reddit.com/r/uBlockOrigin/comments/6smcp4/ublock_origin_is_allowing_ads_in_on_several/

--- contentscript.js
+++ contentscript.js
@@ -325,6 +325,7 @@ if ( !abort ) {
             'stereogum.com',
             'sun-sentinel.com',
             'tf2outpost.com',
+            'thebalance.com',
             'thefrisky.com',
             'thespruce.com',
             'thesuperficial.com',
