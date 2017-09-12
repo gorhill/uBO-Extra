bc55d041e914de7a1ec089ef7919f176b15843e7
gorhill <rhill@raymondhill.net>
2017-09-12 16:05:10 -0400

    add newsarama.com to IL defuser (reported in Chrome store)

--- contentscript.js
+++ contentscript.js
@@ -342,6 +342,7 @@ if ( !abort ) {
             'msn.com',
             'nasdaq.com',
             'nationalpost.com',
+            'newsarama.com',
             'orlandosentinel.com',
             'ottawacitizen.com',
             'ottawasun.com',
