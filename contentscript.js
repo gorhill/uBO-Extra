b15f221d72d844a1e90e10cad2921230918ee316
okiehsch <okiehsch@users.noreply.github.com>
2019-08-21 01:33:46 +0200

    remove broken site

--- contentscript.js
+++ contentscript.js
@@ -243,7 +243,6 @@ if ( !abort ) {
             'marlinmag.com',
             'medicaldaily.com',
             'medicinenet.com',
-            'metacritic.com',
             'metrolyrics.com',
             'mmo-champion.com',
             'momtastic.com',
